1ce3e65c6c9f1ade216a2813f460dc3a77809f41
X1_LAPTOP\seekc <seekchange@daum.net>
2023-05-01 10:41:23 +0900

    실습1 첫번째 제출

---
+++ Lab1/Add.cpp
@@ -0,0 +1,13 @@
+#include "Add.h"
+#include <iostream>
+
+using namespace std;
+
+namespace lab1
+{
+	int Add(int a, int b)
+	{
+		return a + b;
+	}
+}
+
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,15 @@
+#include "add.h"
+#include <iostream>
+
+using namespace lab1;
+using namespace std;
+
 int main()
 {
+	int res = 0;
+	res = Add(3, 5);
 
+	cout << res << endl;
+
+	return 0;
 }
