6f861be17255e85f6fa5479c4e261b63403a9e5a
X1_LAPTOP\seekc <seekchange@daum.net>
2023-05-15 16:15:44 +0100

    lab3 첫번째

---
+++ Lab3/TimeSheet.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+
+namespace lab3
+{
+    class TimeSheet
+    {
+    public:
+        TimeSheet(const char* name, unsigned int maxEntries);
+        ~TimeSheet();
+        TimeSheet(const TimeSheet& other);
+        TimeSheet& operator=(const TimeSheet& rhs);
+        void AddTime(int timeInHours);
+        int GetTimeEntry(unsigned int index) const;
+        int GetTotalTime() const;
+        float GetAverageTime() const;
+        float GetStandardDeviation() const;
+        const std::string& GetName() const;
+
+    private:
+        std::string mName;
+        unsigned int mMaxEntries;
+        int* mWorkTime;
+        unsigned int mNameLength;
+        unsigned int mIndex;
+    };
+}
+
