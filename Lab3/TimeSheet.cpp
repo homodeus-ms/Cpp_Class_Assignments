404b3fd7120aa688e3ea9007dac73401c78a4bf2
X1_LAPTOP\seekc <seekchange@daum.net>
2023-05-15 16:23:26 +0100

    lab3 두번째

--- Lab3/TimeSheet.cpp
+++ Lab3/TimeSheet.cpp
@@ -3,111 +3,111 @@
 
 namespace lab3
 {
-    TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
-        : mMaxEntries(maxEntries)
-        , mNameLength(strlen(name))
-        , mIndex(0)
-        , mName(name)
-    {
-        mWorkTime = new int[mMaxEntries * sizeof(int)];
-        memset(mWorkTime, 0, mMaxEntries * sizeof(int));
-    }
+	TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
+		: mMaxEntries(maxEntries)
+		, mNameLength(strlen(name))
+		, mIndex(0)
+		, mName(name)
+	{
+		mWorkTime = new int[mMaxEntries * sizeof(int)];
+		memset(mWorkTime, 0, mMaxEntries * sizeof(int));
+	}
 
-    TimeSheet::TimeSheet(const TimeSheet& other)
-        : mMaxEntries(other.mMaxEntries)
-        , mNameLength(other.mNameLength)
-        , mIndex(other.mIndex)
-        , mName(other.mName)
-    {
-        mWorkTime = new int[mMaxEntries];
-        memcpy(mWorkTime, other.mWorkTime, mMaxEntries * sizeof(int));
-    }
+	TimeSheet::TimeSheet(const TimeSheet& other)
+		: mMaxEntries(other.mMaxEntries)
+		, mNameLength(other.mNameLength)
+		, mIndex(other.mIndex)
+		, mName(other.mName)
+	{
+		mWorkTime = new int[mMaxEntries];
+		memcpy(mWorkTime, other.mWorkTime, mMaxEntries * sizeof(int));
+	}
 
-    TimeSheet::~TimeSheet()
-    {
-        delete[] mWorkTime;
-    }
-    TimeSheet& TimeSheet::operator=(const TimeSheet& rhs)
-    {
-        mName = rhs.mName;
-        mMaxEntries = rhs.mMaxEntries;
-        memcpy(mWorkTime, rhs.mWorkTime, mMaxEntries * sizeof(int));
-        mNameLength = rhs.mNameLength;
-        mIndex = rhs.mIndex;
+	TimeSheet::~TimeSheet()
+	{
+		delete[] mWorkTime;
+	}
+	TimeSheet& TimeSheet::operator=(const TimeSheet& rhs)
+	{
+		mName = rhs.mName;
+		mMaxEntries = rhs.mMaxEntries;
+		memcpy(mWorkTime, rhs.mWorkTime, mMaxEntries * sizeof(int));
+		mNameLength = rhs.mNameLength;
+		mIndex = rhs.mIndex;
 
-        return *this;
-    }
-    void TimeSheet::AddTime(int timeInHours)
-    {
-        if (timeInHours >= 1 && timeInHours <= 10 && mIndex < mMaxEntries)
-        {
-            mWorkTime[mIndex] = timeInHours;
-            mIndex++;
-        }
-    }
+		return *this;
+	}
+	void TimeSheet::AddTime(int timeInHours)
+	{
+		if (timeInHours >= 1 && timeInHours <= 10 && mIndex < mMaxEntries)
+		{
+			mWorkTime[mIndex] = timeInHours;
+			mIndex++;
+		}
+	}
 
-    int TimeSheet::GetTimeEntry(unsigned int index) const
-    {
-        if (index >= mMaxEntries)
-        {
-            return -1;
-        }
-        return mWorkTime[index] == 0 ? -1 : mWorkTime[index];
-    }
+	int TimeSheet::GetTimeEntry(unsigned int index) const
+	{
+		if (index >= mMaxEntries)
+		{
+			return -1;
+		}
+		return mWorkTime[index] == 0 ? -1 : mWorkTime[index];
+	}
 
-    int TimeSheet::GetTotalTime() const
-    {
-        unsigned int result = 0;
-        for (int i = 0; i < (int)mIndex; ++i)
-        {
-            result += mWorkTime[i];
-        }
+	int TimeSheet::GetTotalTime() const
+	{
+		unsigned int result = 0;
+		for (int i = 0; i < (int)mIndex; ++i)
+		{
+			result += mWorkTime[i];
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    float TimeSheet::GetAverageTime() const
-    {
-        if (mIndex == 0)
-        {
-            return 0.0f;
-        }
-        int sum = GetTotalTime();
-        float average = sum / (float)mIndex;
-        return average;
-    }
+	float TimeSheet::GetAverageTime() const
+	{
+		if (mIndex == 0)
+		{
+			return 0.0f;
+		}
+		int sum = GetTotalTime();
+		float average = sum / (float)mIndex;
+		return average;
+	}
 
-    float TimeSheet::GetStandardDeviation() const
-    {
-        if (mIndex == 0)
-        {
-            return 0.0f;
-        }
-        float averageSquareX;
-        int sumSquareX = 0;
-        int sum = 0;
-        float average;
-        float sDeviation;
+	float TimeSheet::GetStandardDeviation() const
+	{
+		if (mIndex == 0)
+		{
+			return 0.0f;
+		}
+		float averageSquareX;
+		int sumSquareX = 0;
+		int sum = 0;
+		float average;
+		float sDeviation;
 
-        for (int i = 0; i < (int)mIndex; ++i)
-        {
-            int value = mWorkTime[i];
-            sum += value;
-            sumSquareX += value * value;
-        }
+		for (int i = 0; i < (int)mIndex; ++i)
+		{
+			int value = mWorkTime[i];
+			sum += value;
+			sumSquareX += value * value;
+		}
 
-        average = sum / (float)mIndex;
-        averageSquareX = sumSquareX / (float)mIndex;
+		average = sum / (float)mIndex;
+		averageSquareX = sumSquareX / (float)mIndex;
 
-        sDeviation = averageSquareX - average * average;
+		sDeviation = averageSquareX - average * average;
 
-        sDeviation = sqrtf(sDeviation);
+		sDeviation = sqrtf(sDeviation);
 
-        return sDeviation;
-    }
+		return sDeviation;
+	}
 
-    const std::string& TimeSheet::GetName() const
-    {
-        return mName;
-    }
+	const std::string& TimeSheet::GetName() const
+	{
+		return mName;
+	}
 }
--- Lab3/TimeSheet.h
+++ Lab3/TimeSheet.h
@@ -3,26 +3,26 @@
 
 namespace lab3
 {
-    class TimeSheet
-    {
-    public:
-        TimeSheet(const char* name, unsigned int maxEntries);
-        ~TimeSheet();
-        TimeSheet(const TimeSheet& other);
-        TimeSheet& operator=(const TimeSheet& rhs);
-        void AddTime(int timeInHours);
-        int GetTimeEntry(unsigned int index) const;
-        int GetTotalTime() const;
-        float GetAverageTime() const;
-        float GetStandardDeviation() const;
-        const std::string& GetName() const;
+	class TimeSheet
+	{
+	public:
+		TimeSheet(const char* name, unsigned int maxEntries);
+		~TimeSheet();
+		TimeSheet(const TimeSheet& other);
+		TimeSheet& operator=(const TimeSheet& rhs);
+		void AddTime(int timeInHours);
+		int GetTimeEntry(unsigned int index) const;
+		int GetTotalTime() const;
+		float GetAverageTime() const;
+		float GetStandardDeviation() const;
+		const std::string& GetName() const;
 
-    private:
-        std::string mName;
-        unsigned int mMaxEntries;
-        int* mWorkTime;
-        unsigned int mNameLength;
-        unsigned int mIndex;
-    };
+	private:
+		std::string mName;
+		unsigned int mMaxEntries;
+		int* mWorkTime;
+		unsigned int mNameLength;
+		unsigned int mIndex;
+	};
 }
 
