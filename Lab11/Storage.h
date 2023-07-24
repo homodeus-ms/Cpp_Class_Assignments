346548f1d0bcd553747e918f4c840a620bd5d83d
X1_LAPTOP\seekc <seekchange@daum.net>
2023-07-24 16:49:09 +0900

    실습11 첫번째 제출

---
+++ Lab11/Storage.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <memory>
+
+namespace lab11
+{
+	template<typename T>
+	class Storage final
+	{
+	public:
+		Storage(unsigned int length);
+		Storage(unsigned int length, const T& initialValue);
+		Storage() = delete;
+
+		Storage(const Storage& other);
+		Storage& operator=(const Storage& other);
+
+		Storage(Storage&& other);
+		Storage& operator=(Storage&& other);
+
+		~Storage() = default;
+
+		bool Update(unsigned int index, const T& data);
+		const std::unique_ptr<T[]>& GetData() const;
+		unsigned int GetSize() const;
+
+	private:
+		std::unique_ptr<T[]> mPtr;
+		unsigned int mLength;
+	};
+
+	template<typename T>
+	Storage<T>::Storage(unsigned int length)
+		: mLength(length)
+		, mPtr(new T[length])
+	{
+		for (size_t i = 0; i < mLength; ++i)
+		{
+			mPtr[i] = 0;
+		}
+	}
+
+	template<typename T>
+	Storage<T>::Storage(unsigned int length, const T& initialValue)
+		: mLength(length)
+		, mPtr(new T[length])
+	{
+		for (size_t i = 0; i < mLength; ++i)
+		{
+			mPtr[i] = initialValue;
+		}
+	}
+
+	template<typename T>
+	Storage<T>::Storage(const Storage<T>& other)
+		: mLength(other.mLength)
+		, mPtr(new T[other.mLength])
+	{
+		for (size_t i = 0; i < mLength; ++i)
+		{
+			mPtr[i] = other.mPtr[i];
+		}
+	}
+
+	template<typename T>
+	Storage<T>& Storage<T>::operator=(const Storage<T>& other)
+	{
+		if (this != &other)
+		{
+			mPtr.reset();
+
+			mLength(other.mLength);
+
+			mPtr(new T[mLength]);
+
+			for (size_t i = 0; i < mLength; ++i)
+			{
+				mPtr[i] = other.mPtr[i];
+			}
+		}
+
+		return *this;
+	}
+
+	template<typename T>
+	Storage<T>::Storage(Storage<T>&& other)
+		: mLength(other.mLength)
+		, mPtr(std::move(other.mPtr))
+	{
+		other.mLength = 0;
+	}
+
+	template<typename T>
+	Storage<T>& Storage<T>::operator=(Storage<T>&& other)
+	{
+		if (this != &other)
+		{
+			mPtr.reset();
+
+			mPtr = std::move(other.mPtr);
+			mLength = other.mLength;
+
+			other.mLength = 0;
+		}
+
+		return *this;
+	}
+
+	template<typename T>
+	bool Storage<T>::Update(unsigned int index, const T& data)
+	{
+		if (index >= mLength)
+		{
+			return false;
+		}
+
+		mPtr[index] = data;
+
+		return true;
+	}
+
+	template<typename T>
+	const std::unique_ptr<T[]>& Storage<T>::GetData() const
+	{
+		return mPtr;
+	}
+
+	template<typename T>
+	unsigned int Storage<T>::GetSize() const
+	{
+		return mLength;
+	}
+}
